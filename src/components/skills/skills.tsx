921ef57b8079e930ce56f23fa90fc980164b3776
AMendozaMarimon <124471764+AMendozaMarimon@users.noreply.github.com>
2024-06-23 19:25:19 -0500

    Added framer Motion

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -12,6 +12,7 @@ import { FaGitAlt } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import Contact from "../../assets/imgs/Contact.webp";
 import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 // Define el tipo del estado
 interface AppState {
@@ -21,6 +22,26 @@ interface AppState {
 export default function Skills() {
   // Accedo al estado de darkMode
   const darkMode = useSelector((state: AppState) => state.darkMode);
+
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
 
   return (
     <section
@@ -36,56 +57,85 @@ export default function Skills() {
         <img src={Contact} alt="Star" draggable="false" loading="lazy" />
       </div>
       {/* Iconos de habilidades */}
-      <div className="flex flex-wrap justify-center items-center gap-6 text-4xl text-grayText md:gap-10 dark:text-white">
-        <IoLogoJavascript
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorJavascript"
-          title="JavaScript"
-        />
-        <BiLogoTypescript
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorTypescript"
-          title="TypeScript"
-        />
-        <FaHtml5
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorHTML"
-          title="HTML"
-        />
-        <IoLogoCss3
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorCSS"
-          title="CSS"
-        />
-        <DiReact
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorReact"
-          title="React"
-        />
-        <RiTailwindCssFill
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorTailwind"
-          title="Tailwind"
-        />
-        <SiRedux
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorRedux"
-          title="Redux"
-        />
-        <FaNode
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorNode"
-          title="Node"
-        />
-        <SiExpress
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-black"
-          title="Express"
-        />
-        <BiLogoPostgresql
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorPostgresql"
-          title="PostgreSQL"
-        />
-        <FaGitAlt
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-colorGit"
-          title="Git"
-        />
-        <FaGithub
-          className="transition-transform duration-300 transform hover:scale-125 hover:text-black"
-          title="Github"
-        />
-      </div>
+      <motion.ul
+        className="flex flex-wrap justify-center items-center gap-6 text-4xl text-grayText md:gap-10 dark:text-white"
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.li variants={item}>
+          <IoLogoJavascript
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorJavascript"
+            title="JavaScript"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <BiLogoTypescript
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorTypescript"
+            title="TypeScript"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <FaHtml5
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorHTML"
+            title="HTML"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <IoLogoCss3
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorCSS"
+            title="CSS"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <DiReact
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorReact"
+            title="React"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <RiTailwindCssFill
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorTailwind"
+            title="Tailwind"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <SiRedux
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorRedux"
+            title="Redux"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <FaNode
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorNode"
+            title="Node"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <SiExpress
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-black"
+            title="Express"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <BiLogoPostgresql
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorPostgresql"
+            title="PostgreSQL"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <FaGitAlt
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-colorGit"
+            title="Git"
+          />
+        </motion.li>
+        <motion.li variants={item}>
+          <FaGithub
+            className="transition-transform duration-300 transform hover:scale-125 hover:text-black"
+            title="Github"
+          />
+        </motion.li>
+      </motion.ul>
     </section>
   );
 }
